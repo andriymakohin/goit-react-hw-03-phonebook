48e33111fa8d44254c3baf9ae88b1eab1b0b8045
Andrii Makohin <andreas2010@ukr.net>
2020-08-20 10:32:07 +0300

    Updates

---
+++ precache-manifest.c13a25c6459455b9b16b52aa63d342c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e857c855335974606264c64008d154e4",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "0e8d05bfade527ef7cde",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.16e10af3.chunk.css"
+  },
+  {
+    "revision": "5902ed1b798519c6d74d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.59ec1e6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.59ec1e6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8d05bfade527ef7cde",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.6b6a9c1f.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
